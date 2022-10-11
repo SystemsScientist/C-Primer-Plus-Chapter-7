84fdad66fc8cb171b39819310c822d18ceb1ce7a
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-10-11 07:59:09 -0500

    Adding Listing-7-3

---
+++ Listing-7-3/cypher2.c
@@ -0,0 +1,20 @@
+// cypher2.c -- alters input, preserving non-letters
+
+#include <stdio.h>
+#include <ctype.h>	// for isaplha
+
+int main() {
+
+	char ch;
+
+	while((ch = getchar()) != '\n') {
+
+		if (isalpha(ch))		// if a letter,
+			putchar(ch + 1);	// change it
+		else				// otherwise,
+			putchar(ch);		// print as is
+	}
+	putchar(ch);				// print the newline
+
+	return 0;
+}
